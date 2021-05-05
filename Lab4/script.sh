87bac9e178aa9f5517b6c476327716e077cd525d
SkyPwner <55462569+SkyPwner@users.noreply.github.com>
2021-05-05 21:10:56 +0300

    Update script.sh

--- Lab4/script.sh
+++ Lab4/script.sh
@@ -1,10 +1,10 @@
 g++ lab4_1.cpp -Wall -o lab4_ex
 g++ lab4_2.cpp -Wall -o lab4_2
 g++ lab4_3.cpp -Wall -o lab4_3
-RED='\033[0;31m'
-NC='\033[0m'
-echo "\033[0;31m\t Fork example \033[0m"
+echo "Fork example"
 ./lab4_ex 1 2 3
-echo "\n\n\033[0;31m\t Clone example \033[0m"
+echo "Fork example"
 ./lab4_2 1 2 3 4
+echo "Clone example"
+./lab4_3 9 8 9 4 
 rm lab4_ex lab4_2 lab4_3
