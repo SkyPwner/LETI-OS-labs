f63e703dd29a56877f091e0bc83bcdc0f25abe88
SkyPwner <55462569+SkyPwner@users.noreply.github.com>
2021-05-05 21:22:25 +0300

    Update script.sh

--- Lab4/script.sh
+++ Lab4/script.sh
@@ -1,7 +1,7 @@
 g++ lab4_1.cpp -Wall -o lab4_ex
 g++ lab4_2.cpp -Wall -o lab4_2
 g++ lab4_3.cpp -Wall -o lab4_3
-echo "Fork example"
+echo "Lab4 args example"
 ./lab4_ex 1 2 3
 echo "Fork example"
 ./lab4_2 1 2 3 4
